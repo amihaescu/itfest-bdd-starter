ddc9df60a1861e72bb6ea9b68dfcb9b8fc2c7ce0
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2016-11-19 10:46:11 +0200

    Added some more number tests

--- src/main/java/com/db/gt/itfest/numbers/Numbers.java
+++ src/main/java/com/db/gt/itfest/numbers/Numbers.java
@@ -2,12 +2,18 @@ package com.db.gt.itfest.numbers;
 
 import java.util.List;
 
-class Average {
+class Numbers {
 
     private List<Integer> array;
+    private int a,b;
 
-    Average(List<Integer> array) {
+    Numbers(List<Integer> array) {
         this.array = array;
+    }
+
+    Numbers(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
 
     float calculate() {
@@ -22,7 +28,7 @@ class Average {
      * Given an array of ints, return true if one of the first 4 elements in the array is a 9.
      * The array length may be less than 4.
      */
-    public boolean arrayFrontNine() {
+    boolean arrayFrontNine() {
         int end = array.size();
         if (end > 4) end = 4;
 
@@ -31,5 +37,21 @@ class Average {
         }
 
         return false;
+    }
+
+    public int closerTo(Integer closer) {
+        int aDiff = Math.abs(a - closer);
+        int bDiff = Math.abs(b - closer);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;  // i.e. aDiff == bDiff
+
+        // Solution notes: aDiff/bDiff local vars clean the code up a bit.
+        // Could have "else" before the second if and the return 0.
     }
 }
---
+++ src/test/java/com/db/gt/itfest/numbers/NumbersStepDefinition.java
@@ -0,0 +1,57 @@
+package com.db.gt.itfest.numbers;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumbersStepDefinition {
+
+    private Numbers numbers;
+    private float result;
+    private boolean isPresent;
+    private Integer closerTo;
+
+    @Given("^I have an numbers object with the values: (.*)")
+    public void initialize(List<Integer> list) {
+        numbers = new Numbers(list);
+    }
+
+    @Given("I have an numbers object with a=(\\d+) and b=(\\d+)")
+    public void initialize(Integer a, Integer b){
+        numbers = new Numbers(a, b);
+    }
+
+    @When("^I calculate the average")
+    public void calculateAvg(){
+        result = numbers.calculate();
+    }
+
+    @When("I check if one of the first 4 elements in the array is a 9")
+    public void checkNineIsPresent(){
+        isPresent = numbers.arrayFrontNine();
+    }
+
+    @When("I check to see which one is closer to (\\d+)")
+    public void check(Integer c){
+        closerTo = numbers.closerTo(c);
+    }
+
+    @Then("^I expect it to be (.*)")
+    public void expectAvg(float expected){
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Then("the result should be (.*)")
+    public void expectBooleanResult(Boolean b){
+        assertEquals(b, isPresent);
+    }
+
+    @Then("the closer one should be (\\d+)")
+    public void expectTheCloserOne(Integer closerOne){
+        assertEquals(closerOne, closerTo);
+    }
+}
